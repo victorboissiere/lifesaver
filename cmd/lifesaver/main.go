616a1ddfbba0c064d3cf40899a6c6296a8ca6cbb
Victor Boissiere <victor.boissiere@gmail.com>
2018-05-20 22:58:21 +0200

    Add travis ci

--- cmd/lifesaver/main.go
+++ cmd/lifesaver/main.go
@@ -6,15 +6,18 @@ import (
 	"io/ioutil"
 	"log"
 	"gopkg.in/yaml.v2"
+	"os/exec"
+	"strings"
 )
 
-type Config map[string]struct {
-	Description string
-	Programs []string
+type Config map[string]Installation
+type Installation struct {
+	Description  string
+	Programs     []string
 	Dependencies []string // optional
 	Steps []struct {
 		Description string
-		Commands interface{}
+		Commands    interface{}
 	}
 }
 
@@ -42,13 +45,34 @@ func printUsage(config Config) {
 	os.Exit(1)
 }
 
+func installPrograms(programs []string) {
+	for _, program := range programs {
+		fmt.Printf("[PROGRAM] %s\n", strings.ToUpper(program))
+		cmd := exec.Command("apt", "install", "-y", program)
+		if out, err := cmd.CombinedOutput(); err != nil {
+			log.Fatalf("Stdout: %sFailed with %s\n", out, err)
+		}
+	}
+}
+
+func install(installation Installation) {
+	fmt.Println("[PROGRAMS] Installing")
+	installPrograms(installation.Programs)
+}
+
 func main()  {
 	config := getConfig()
 
 	if len(os.Args) != 2 {
 		printUsage(config)
 	}
-	installCommand := os.Args[1]
 
-	fmt.Printf("Install mode: %s", installCommand)
+	installCommand := os.Args[1]
+	installation, isValid := config[installCommand]
+	if !isValid {
+		printUsage(config)
+	}
+
+	fmt.Printf("====> Installing '%s'\n", installCommand)
+	install(installation)
 }
