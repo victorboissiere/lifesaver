23a4e2ad4bb068f89b23bf4656f67b5d86f6fa1d
Victor Boissiere <victor.boissiere@gmail.com>
2018-05-24 23:04:46 +0200

    Fix wget

--- installer/utils.go
+++ installer/utils.go
@@ -20,7 +20,7 @@ func execCommand(command string) {
 }
 
 func DownloadFile(url string, filename string) {
-	execCommand(fmt.Sprintf("wget -O %s %s", url, filename))
+	execCommand(fmt.Sprintf("wget -O %s %s", filename, url))
 }
 
 func createPathIfNotExists(filename string) {
