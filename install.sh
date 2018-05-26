3a3fe2fe395b00c3e5e8dc3e29fe50d724b3f3cb
Victor Boissiere <victor.boissiere@gmail.com>
2018-05-26 17:35:40 +0200

    Add binary version in install script

--- install.sh
+++ install.sh
@@ -17,9 +17,10 @@ fi
 
 echo "Please provide sudo pass for software install (simple sudo echo trigger):"
 sudo echo -e "Thanks! All set!\n"
-echo "Download latest binary..."
+echo "Downloading latest binary version $(wget -q -O - https://api.github.com/repos/victorboissiere/lifesaver/releases/latest  | grep tag_name | cut -d '"' -f 4)"
 wget -q -O /tmp/lifesaver "$(wget -q -O - https://api.github.com/repos/victorboissiere/lifesaver/releases/latest | grep browser_download_url | cut -d '"' -f 4)"
 echo -e "Done!\n"
+
 chmod +x /tmp/lifesaver
 /tmp/lifesaver $@
 rm /tmp/lifesaver
